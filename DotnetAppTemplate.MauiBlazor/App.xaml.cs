b4e0cf3ceda28eb778b4b4beaa2a35ae2a88a120
Kitfka <philipthuys@gmail.com>
2023-09-07 15:59:56 +0200

    Add a mauiblazor web and commen

---
+++ DotnetAppTemplate.MauiBlazor/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace DotnetAppTemplate.MauiBlazor;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new MainPage();
+    }
+}
---
+++ DotnetAppTemplate.MauiBlazor/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace DotnetAppTemplate.MauiBlazor;
+
+public partial class MainPage : TabbedPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
